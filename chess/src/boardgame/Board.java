1a1cabc17714f8fd00a3d7bc2de23d796a5f7efe
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-16 23:21:22 -0300

    added: chess layer and printing the board

--- chess/src/boardgame/Board.java
+++ chess/src/boardgame/Board.java
@@ -28,4 +28,12 @@ public class Board {
         this.columns = columns;
     }
 
+    public Piece getPiece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece getPiece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 }
