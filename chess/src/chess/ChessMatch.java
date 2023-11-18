6a60ce5e05829bb389a0c804dd52243b64b3199c
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-17 23:25:50 -0300

    added: Little improvement in board printing

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -26,12 +26,22 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece(6, 'b', new Rook(board, Color.WHITE));
-        placeNewPiece(8, 'e', new King(board, Color.BLACK));
-        placeNewPiece(1, 'e', new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 
-    private void placeNewPiece(int row, char column, ChessPiece chessPiece) {
+    private void placeNewPiece(char column, int row, ChessPiece chessPiece) {
         board.placePiece(chessPiece, new ChessPosition(row, column).toPosition());
     }
 
