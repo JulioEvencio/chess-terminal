ebbb19c8544041f49af45054d7491e72d6b8b2d3
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-24 23:43:13 -0300

    added: piece move count

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -178,7 +178,10 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+
+        p.increaseMoveCount();
+
         Piece capturedPiece = board.removePiece(target);
 
         board.placePiece(p, target);
@@ -192,7 +195,9 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+
+        p.decreaseMoveCount();
 
         board.placePiece(p, source);
 
